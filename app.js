7ea8182ef7345705b9b5542814ba5ae56a1dd3bc
May Carandang <maytiara@gmail.com>
2022-11-07 02:07:57 +0800

    moved the rest of pseudo codes to ./public/js pie-chart file

--- app.js
+++ app.js
@@ -21,14 +21,5 @@ app.listen(PORT, () => {
 });
 
 
-// create a single page for pie-chart with random dataset
-// pie chart using D3 library
-// create json file with 10-20 dataset with additional object attributes
 
-// add regenerate button > once onClick new data will be generated 
-// the new generated chart w/ 10 random data need to reflect the UI of it value
 
-// have it responsive by fit the entire page and resize when it's on mobile mode
-// add media query for diff media screen
-
-// convert to typescript file
