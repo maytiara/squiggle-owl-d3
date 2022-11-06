17c57c306f9eeedaaef25fb1e4daf1ab548178de
May Carandang <maytiara@gmail.com>
2022-11-06 22:12:12 +0800

    to declare the files inside the public folder using 'path' & added express middleware with wildcard get req

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // 1st: add the required packages
 const express = require('express'); // express.js
+const path = require ('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -7,8 +8,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to Parse URL data from any form
 
-// Subdirectory 'public/css/'
-app.use(express.static(__dirname));
+// Subdirectory 'public/..'
+app.use(express.static(path.join(__dirname,'public')));
+
+// Wildcard for general get req of the app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public'))});
 
 // Server setup
 app.listen(PORT, () => {
