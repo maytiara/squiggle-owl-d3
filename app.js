f58574e121b4b5cba6f911d2e4ad7961f5e37195
May Carandang <maytiara@gmail.com>
2022-11-08 17:48:01 +0800

    added get entry point for this test app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 1st: add the required packages
 const express = require('express'); // express.js
-const path = require ('path');
+const { join } = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,17 +9,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to Parse URL data from any form
 
 // Subdirectory 'public/..'
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(join(__dirname,'public')));
 
 // Wildcard for general get req of the app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public'))});
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'public/index'))});
 
 // Server setup
 app.listen(PORT, () => {
   console.log(`Data Visualisation listening on ${PORT} 🚀`);
 });
-
-
-
-
